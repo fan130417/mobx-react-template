c71a693cd104ab4ebd25beddd435022197069883
FanJunjie <fandl0417@163.com>
2021-09-25 23:21:28 +0800

    feat: 优化

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.less';
+
+export default class RootPage extends Component {
+
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+
+}
--- src/router.js
+++ src/router.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from "react-router-dom";
 import RootPage from 'containers/RootPage';
+import HomePage from 'components/HomePage';
 
 export default function () {
     return (
         <Router>
             <RootPage >
                 <Switch>
-                    <Route path='/dashboard/exhibition' component={MyExhibitions} />
-                    <Route path='/dashboard/artwork' component={MyArtworks} />
                     <Route exact path='/' component={HomePage} />
                     <Redirect from='*' to='/' />
                 </Switch>
